2b609e85eea36a178425febb14db8a5b0620bebc
Jesse Wenger <wengerj@gmail.com>
2021-03-10 05:14:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,8 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
