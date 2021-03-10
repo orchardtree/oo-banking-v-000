bd606ba535e9226bdb3d006931ab356fcff144fd
Jesse Wenger <wengerj@gmail.com>
2021-03-10 05:21:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   
   def execute_transaction
     if @status == "pending" && valid?
-      #binding.pry
+      binding.pry
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
