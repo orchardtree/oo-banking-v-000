a6b995757489c73b9337ec658ee434fc0fd319b1
Jesse Wenger <wengerj@gmail.com>
2021-03-10 04:57:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   # your code here
-  attr_accessor :sender, :receiver
+  attr_accessor :sender, :receiver, :status
     
   def initialize(sender, receiver, amount)
     @sender = sender
